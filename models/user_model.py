2387663cd96c17b3fc5bc9e60ad1003c38780bac
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-16 21:47:10 +0100

    Added user model

---
+++ models/user_model.py
@@ -0,0 +1,31 @@
+import datetime
+
+
+class UserModel(object):
+    """ This class handles all model operations to the User"""
+    user = []
+
+    def __init__(self, first_name, last_name, email, password ):
+        """
+        This constructor initialises all the parameters
+        :param first_name: 
+        :param last_name: 
+        :param email: 
+        :param password: 
+        """
+        self.user_id = len(UserModel.user)+1
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.date_created = datetime.datetime.utcnow()
+        self.date_modified = None
+
+    def create_user(self):
+        """
+        Adds user model object to user list
+        :return: 
+        """
+        UserModel.user.append(self)
+        return self.user_id
+
