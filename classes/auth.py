c60dafacdbdeb7d8d78997d8448260a0a2de7865
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-16 15:17:30 +0100

    Added class to handle authentication

---
+++ classes/auth.py
@@ -0,0 +1,12 @@
+from flask import jsonify
+from validate_email import validate_email
+
+class Authentication(object):
+    """
+    This class handles user registration.
+    Login and Registration
+    """
+
+    @staticmethod
+    def registration(first_name, last_name, email, password):
+        pass
