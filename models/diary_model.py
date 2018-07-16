6b627d83955e3a48811318263c96f1e72c61f9a2
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-16 21:43:33 +0100

    Added constructor and method to add user object

--- models/diary_model.py
+++ models/diary_model.py
@@ -4,4 +4,28 @@ import datetime
 class DiaryModel(object):
     """ This class handles all model operations to the diary"""
     diary = []
-    
+
+    def __init__(self, first_name, last_name, email, password ):
+        """
+        This constructor initialises all the parameters
+        :param first_name: 
+        :param last_name: 
+        :param email: 
+        :param password: 
+        """
+        self.diary_id = len(DiaryModel.diary)+1
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.date_created = datetime.datetime.utcnow()
+        self.date_modified = None
+
+    def create_diary(self):
+        """
+        Adds diary model object to diary list
+        :return: 
+        """
+        DiaryModel.diary.append(self)
+        return self.diary_id
+
