e1b9b9da65af3f7b28a7a7295570527d5fdcf9f6
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-16 16:57:11 +0100

    Added file to contain all dummy data for tests and setup'

---
+++ tests/test_base.py
@@ -0,0 +1,9 @@
+import unittest
+from flask import json
+from api.diary import app
+from instance.config import application_config
+
+class BaseClass(unittest.TestCase):
+    def setUp(self):
+        app.config.from_object(application_config['TestingEnv'])
+        self.client = app.test_client()
