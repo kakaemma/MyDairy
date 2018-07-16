6ecf1aab57d3c71be64ce2c534b2d34a4e7482b4
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-16 22:06:28 +0100

    Clened up code

--- models/user_model.py
+++ models/user_model.py
@@ -31,14 +31,26 @@ class UserModel(object):
 
     @staticmethod
     def check_if_email_exists(email):
+        """
+        This method checks if an email already exists
+        :param email: 
+        :return: 
+        """
         for this_user in UserModel.user:
             if this_user.email == email:
                 return this_user.email
 
     @staticmethod
     def check_if_user_is_valid(email, password):
+        """
+         This method checks if the email and 
+         password exist in the system
+         
+        :param email: 
+        :param password: 
+        :return: 
+        """
         for this_user in UserModel.user:
             if this_user.email == email and\
                             this_user.password == password:
                 return this_user.user_id
-            
