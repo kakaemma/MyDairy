8a81c1d39310f79f8ce541f28b8d570532a4e088
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-16 21:08:53 +0100

    Added validatrions for registration end point

--- classes/auth.py
+++ classes/auth.py
@@ -18,4 +18,18 @@ class Authentication(object):
         :param password: 
         :return: 
         """
-        pass
+        if not first_name or not last_name\
+                or not email or not password:
+            response = jsonify({'Error':'Missing values'})
+            response.status_code = 400
+            return response
+
+        if not validate_email(email):
+            response = jsonify({'Conflict': 'Invalid email address'})
+            response.status_code = 400
+            return response
+
+        if len(password) < 5:
+            response = jsonify({'Error': 'Password too short'})
+            response.status_code = 422
+            return response
