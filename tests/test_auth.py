5e09c1b0f0831d46260ae7cbe5892fa8d04da772
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-17 13:35:30 +0100

    Adde test for login with invalid credentials

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -64,3 +64,12 @@ class TestAuthentication(BaseClass):
                       response.data.decode())
         self.assertEqual(response.status_code, 400)
 
+    def test_login_with_invalid_credentials(self):
+        self.client.post('/api/v1/register',
+                                    data=self.user)
+        response = self.client.post('/api/v1/login',
+                                    data=self.invalid_user)
+        self.assertIn('Invalid login credentials',
+                      response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
