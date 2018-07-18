3a9ca9335839c76467d007afd44a00da6e2e2211
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-19 00:57:31 +0100

    Added mehtods to retrieve descriptions

--- classes/item.py
+++ classes/item.py
@@ -79,5 +79,56 @@ class DiaryItem(object):
             response.status_code = 400
             return response
 
+        edit_item = ItemModel.modify_description(diary_id, item_id, desc)
+        if not edit_item:
+            response = jsonify(
+                {
+                    'Error': 'Can not edit item with same description'
+                })
+            response.status_code = 400
+            return response
+
+        response = jsonify({'info': 'Description changed'})
+        response.status_code = 200
+        return response
+
+    @classmethod
+    def get_diary_descriptions(cls, diary_id):
+        if not diary_id:
+            response = jsonify({'Error': 'Diary id missing'})
+            response.status_code = 400
+            return response
+
+        diaries = DiaryModel.check_for_diaries()
+        if not diaries:
+            response = jsonify({'Info': 'Can not retrieve \
+            description on empty diary'})
+            response.status_code = 400
+            return response
+
+        items = ItemModel.check_for_items()
+        if not items:
+            response = jsonify({'Info': 'No descriptions added'})
+            response.status_code = 400
+            return response
+
+        entry_exists = DiaryModel.check_diary_by_id(diary_id)
+        if not entry_exists:
+            response = jsonify(
+                {'Error': 'Attempting to retrieve\
+                 on non existing entry'})
+            response.status_code = 400
+            return response
+
+        diary_desc = ItemModel.get_descriptions(diary_id)
+        response = jsonify({
+            'Diary with id': diary_id,
+            'Descriptions':diary_desc
+        })
+        response.status_code = 200
+        return response
+
+
+
 
 
