3397078bb1e3b5a96120ffb863c0861acefd8d70
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-18 08:39:48 +0100

    Added methods to check wether diary is empty and also get diaries

--- models/diary_model.py
+++ models/diary_model.py
@@ -21,3 +21,22 @@ class DiaryModel(object):
         """
         DiaryModel.diary.append(self)
         return self.diary_id
+
+    @staticmethod
+    def check_for_diaries():
+        """ This checks whether there are any diary entries"""
+        if len(DiaryModel.diary) >=1:
+            return len(DiaryModel.diary)
+
+    @staticmethod
+    def get_diaries(self):
+        all_diaries = []
+        for this_diary in DiaryModel.diary:
+            all_diaries.append({
+                'id': this_diary.diary_id,
+                'name': this_diary.name,
+                'Date created': this_diary.date_created,
+                'Date Modified': this_diary.date_modified
+
+            })
+        return all_diaries
