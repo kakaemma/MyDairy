0507e09dfbf47a6178105c60cd7ea573855af95c
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-19 00:10:20 +0100

    Added method to edit item description

--- models/item_model.py
+++ models/item_model.py
@@ -5,7 +5,7 @@ class ItemModel(object):
 
     description = []
 
-    def __init__(self, diary_id, description):
+    def __init__(self, diary_id, desc):
         """
         This constructor creates an ItemModel object
         :param diary_id: 
@@ -13,9 +13,22 @@ class ItemModel(object):
         """
         self.item_id = len(ItemModel.description)+1
         self.diary_id = diary_id
+        self.desc = desc
         self.date_created = datetime.datetime.utcnow()
         self.date_modified = None
 
     def create_description(self):
         ItemModel.description.append(self)
         return self.item_id
+
+    @staticmethod
+    def modify_description(diary_id, item_id, desc):
+        for this_item in ItemModel.description:
+            if this_item.diary_id == diary_id and \
+                            this_item.item_id == item_id:
+                if this_item.desc == desc
+                    return None
+
+                this_item.desc = desc
+                this_item.date_modified = datetime.datetime.utcnow()
+                return this_item.item_id
