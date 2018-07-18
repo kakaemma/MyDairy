644b89728803eb051bed0fad451ef5f9a75de711
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-18 12:29:48 +0100

    cleaned up code

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -23,25 +23,42 @@ class TestDiary(BaseClass):
         self.assertEqual(response.status_code, 409)
 
     def test_get_diaries_on_empty_Diary(self):
+        """ Should return no diary entries available"""
         response = self.client.get('/api/v1/diary')
         self.assertIn('No diary entries available', response.data.decode())
         self.assertEqual(response.status_code, 404)
 
     def test_get_single_diary_on_empty_diary(self):
+        """ Should return No diary entries added"""
         response = self.client.get('/api/v1/diary/11')
-        self.assertIn('No diary entries added', response.data.decode())
+        self.assertIn('Attempting to retrieve on empty diary',
+                      response.data.decode())
         self.assertEqual(response.status_code, 400)
 
     def test_get_single_diary_with_no_id(self):
+        """ Should return missing diary id"""
         response = self.client.get('/api/v1/diary/0')
         self.assertIn('Missing diary id', response.data.decode())
         self.assertEqual(response.status_code, 400)
 
     def test_get_single_diary_that_does_not_exist(self):
+        """ Should return diary not found and status code 404"""
         self.client.post('/api/v1/diary', data=self.new_diary_2)
         response = self.client.get('/api/v1/diary/45')
         self.assertIn('Diary does not exist', response.data.decode())
         self.assertEqual(response.status_code, 404)
+
+    def test_get_single_diary_successfully(self):
+        """ Should return diary retrieved and status code 200"""
+        self.client.post('/api/v1/diary', data=self.new_diary_2)
+        response = self.client.get('/api/v1/diary/1')
+        self.assertIn('Diary retrieved', response.data.decode())
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
 
 
 
