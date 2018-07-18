d55f454945fec6c1c3dd2dd7475e460a4892b1f2
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-18 11:34:20 +0100

    Added methods to get single diary

--- classes/diary.py
+++ classes/diary.py
@@ -18,7 +18,8 @@ class Diary(object):
             return response
 
         new_diary = DiaryModel(name)
-        if new_diary:
+        added_diary = new_diary.create_diary()
+        if added_diary:
             response = jsonify({
                 'info': 'Diary successfully added',
                             })
@@ -46,4 +47,40 @@ class Diary(object):
         response = jsonify({'Info': 'No diary entries available'})
         response.status_code = 404
         return response
+
+    @classmethod
+    def get_diary(cls, diary_id):
+
+        if not diary_id:
+            response = jsonify({'Error': 'Missing diary id'})
+            response.status_code = 400
+            return response
+
+        diary_is_not_empty = DiaryModel.check_for_diaries()
+        if diary_is_not_empty:
+            single_diary = DiaryModel.check_diary_by_id(diary_id)
+            if not single_diary:
+                response = jsonify({'Info': 'Diary does not exist'})
+                response.status_code = 404
+                print(single_diary)
+
+                return response
+
+            this_diary = DiaryModel.get_diary_by_id(diary_id)
+
+            response = jsonify({
+                'Info': 'Diary retrieved',
+                'Dairy': this_diary
+            })
+            response.status_code = 200
+            return response
+
+
+
+
+        response = jsonify({'Info': 'No diary entries added'})
+        response.status_code = 400
+        return response
+
+
 
