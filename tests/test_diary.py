5be673c1f981d0b911176730c22183935a724d2e
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-18 07:37:30 +0100

    Added tests for adding diary successfully

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -3,5 +3,15 @@ from tests.test_base import BaseClass
 
 class TestDiary(BaseClass):
 
-    def test_add_diary(self):
-        pass
+    def test_add_diary_without_name(self):
+        response = self.client.post('/api/v1/diary',
+                                    data=self.empty_diary)
+        self.assertIn('Missing diary name',
+                         response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_diary_successfully(self):
+        response = self.client.post('/api/v1/diary',
+                                    data=self.new_diary)
+        self.assertIn('Diary successfully added', response.data.decode())
+        self.assertEqual(response.status_code, 201)
