20a70b7bffc5ce901a601f6abb2b36f5b36b3cb6
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-18 08:37:31 +0100

    Added tests for getting diaries on empty diary

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -15,3 +15,9 @@ class TestDiary(BaseClass):
                                     data=self.new_diary)
         self.assertIn('Diary successfully added', response.data.decode())
         self.assertEqual(response.status_code, 201)
+
+    def test_get_diaries_on_empty_Diary(self):
+        response = self.client.get('/api/v1/diary')
+        self.assertIn('No diary entries available', response.data.decode())
+        self.assertEqual(response.status_code, 404)
+
