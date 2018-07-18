2c251d2ab1dbfeb2489cd5d79035f83fba5dcd56
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-19 00:09:29 +0100

    Added validations for modifying a descriprion

--- classes/item.py
+++ classes/item.py
@@ -45,5 +45,39 @@ class DiaryItem(object):
             response.status_code = 201
             return response
 
+    @staticmethod
+    def edit_diary_description(diary_id, item_id, desc):
+        if not diary_id or not item_id:
+            response = jsonify({'Error': 'Missing id for resource(s)'})
+            response.status_code = 400
+            return response
+
+        if not desc:
+            response = jsonify({'Error': 'Missing \
+            description for diary item'})
+            response.status_code = 400
+            return response
+
+        if len(desc) < 10:
+            response = jsonify({'Error': 'Description must have\
+             a minimum of 10 characters'})
+            response.status_code = 400
+            return response
+
+        check_diaries = DiaryModel.check_for_diaries()
+        if not check_diaries:
+            response = jsonify({'Info': 'Can not edit \
+            description on empty diary'})
+            response.status_code = 400
+            return response
+
+        check_entry_exists = DiaryModel.check_diary_by_id(diary_id)
+        if not check_entry_exists:
+            response = jsonify(
+                {'Error': 'Attempting to modify description\
+                 on non existing entry'})
+            response.status_code = 400
+            return response
+
 
 
