5cdb9a457f600585d6596e18994cd9b385177d90
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-19 10:12:32 +0100

    Added tests for modifying a description

--- tests/test_item.py
+++ tests/test_item.py
@@ -54,6 +54,49 @@ class TestDiaryDescription(BaseClass):
              a minimum of 10 characters', response.data.decode())
         self.assertEqual(response.status_code, 400)
 
+    def test_edit_diary_on_empty_diary(self):
+        response = self.client.put('/api/v1/diary/1/item/1', data=self.desc)
+        self.assertIn('Can not edit \
+            description on empty diary', response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_edit_non_existing_diary_entry(self):
+        self.client.post('/api/v1/diary', data=self.new_diary_2)
+        response = self.client.put('/api/v1/diary/5/item/1', data=self.desc2)
+        self.assertIn('Attempting to modify description\
+                 on non existing diary entry', response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_edit_description_on_empty_desc(self):
+        self.client.post('/api/v1/diary', data=self.new_diary_2)
+        response = self.client.put('/api/v1/diary/1/item/1', data=self.desc2)
+        self.assertIn('No descriptions added', response.data.decode())
+        self.assertEqual(response.status_code, 400)
+
+    def test_edit_non_existing_description(self):
+        self.client.post('/api/v1/diary', data=self.new_diary_2)
+        self.client.post('/api/v1/diary/1/item', data=self.desc)
+        response = self.client.put('/api/v1/diary/1/item/4', data=self.desc2)
+        self.assertIn('Description with \
+            that id does not exist', response.data.decode())
+        self.assertEqual(response.status_code, 404)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
