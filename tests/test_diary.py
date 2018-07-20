a8e2257f59507064eb96de77c6ead5941095077a
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-20 01:07:25 +0100

    Added header to tests thst get diaries

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -51,7 +51,7 @@ class TestDiary(BaseClass):
 
     def test_get_diaries_on_empty_Diary(self):
         """ Should return no diary entries available"""
-        response = self.client.get('/api/v1/diary')
+        response = self.client.get('/api/v1/diary', headers=self.header)
         self.assertIn('No diary entries available', response.data.decode())
         self.assertEqual(response.status_code, 404)
 
@@ -61,21 +61,21 @@ class TestDiary(BaseClass):
                          content_type='application/json',
                          data=self.new_diary,
                          headers=self.header)
-        response = self.client.get('/api/v1/diary')
+        response = self.client.get('/api/v1/diary', headers=self.header)
         self.assertIn('My Diary entries', response.data.decode())
         self.assertEqual(response.status_code, 200)
 
 
     def test_get_single_diary_on_empty_diary(self):
         """ Should return No diary entries added"""
-        response = self.client.get('/api/v1/diary/11')
+        response = self.client.get('/api/v1/diary/11', headers=self.header)
         self.assertIn('Attempting to retrieve on empty diary',
                       response.data.decode())
         self.assertEqual(response.status_code, 400)
 
     def test_get_single_diary_with_no_id(self):
         """ Should return missing diary id"""
-        response = self.client.get('/api/v1/diary/0')
+        response = self.client.get('/api/v1/diary/0', headers=self.header)
         self.assertIn('Missing diary id', response.data.decode())
         self.assertEqual(response.status_code, 400)
 
@@ -85,7 +85,7 @@ class TestDiary(BaseClass):
                          content_type='application/json',
                          data=self.new_diary_2,
                          headers=self.header)
-        response = self.client.get('/api/v1/diary/45')
+        response = self.client.get('/api/v1/diary/45', headers=self.header)
         self.assertIn('Diary does not exist', response.data.decode())
         self.assertEqual(response.status_code, 404)
 
@@ -95,7 +95,7 @@ class TestDiary(BaseClass):
                          content_type='application/json',
                          data=self.new_diary_2,
                          headers=self.header)
-        response = self.client.get('/api/v1/diary/1')
+        response = self.client.get('/api/v1/diary/1', headers=self.header)
         self.assertIn('Diary retrieved', response.data.decode())
         self.assertEqual(response.status_code, 200)
 
