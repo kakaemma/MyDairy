329a06bdca9c5e8b8b319002838536e61b376a8c
Emmanuel Kakaire <kakaemma1@gmail.com>
2018-07-22 23:26:20 +0100

    Remove logging from application

--- api/__init__.py
+++ api/__init__.py
@@ -1,21 +1,10 @@
 from flask_api import FlaskAPI
 from instance.config import application_config
-import logging
-from logging.handlers import RotatingFileHandler
 
 
-formatter = logging.Formatter(
-    "[%ascitime]s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s} "
-)
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(application_config[config_name])
     app.config.from_pyfile('config.py')
-
-    file_handler = RotatingFileHandler(app.config.get('LOGFILE'),
-                                  maxBytes=10000000, backupCount=5)
-    file_handler.setFormatter(formatter)
-    app.logger.setLevel(app.config.get('LEVEL'))
-    app.logger.addHandler(file_handler)
 
     return app
